7d603effe326364c262b74aaca47a5c0ef17ae7c
P4W4R <rbhagawa@LIN59017954.corp.capgemini.com>
2021-09-20 10:52:00 +0530

    Merge branch 'reeportfeature'

--- sampleapp/src/main/java/org/cap/demo/BookClass.java
+++ sampleapp/src/main/java/org/cap/demo/BookClass.java
@@ -3,5 +3,9 @@ package org.cap.demo;
 public class BookClass {
 	public static void main(String[] args) {
 		System.out.println("Hello, from sts");
+		show();
+	}
+	public static void show() {
+		System.out.println("Show here!");
 	}
 }
